aa68a0ed5a59fd88731891c4a8c85884d97e9a78
Erik Seliger <erikseliger@me.com>
2024-09-17 11:54:30 -0400

    Add highlight queries for syntax highlighting

---
+++ queries/highlights.scm
@@ -0,0 +1,42 @@
+["import" "fn" "from" "let" "set" "struct" "while" "if" "else" "return" "variant" "match"] @keyword
+
+(binary_e ["&&" "||" "==" "!=" "<" "<=" ">" ">=" "+" "-" "*" "/"] @identifier.operator)
+
+(top_func name: (lower_ident) @identifier.function)
+
+[(int_lit) (float_lit)] @number
+(bool_lit) @boolean
+(bytes_lit) @string
+
+(top_import 
+	internal: (lower_ident) @identifier.function
+	external: (lower_ident) @identifier.function)
+
+[(ty_i32) (ty_f32) (ty_bytes) (ty_bool) (ty_unit)] @type.builtin
+
+(var_e) @identifier
+(let_decl binder: (lower_ident) @identifier)
+(set_var) @identifier
+
+(top_struct name: (upper_ident) @identifier.type)
+(top_variant name: (upper_ident) @identifier.type)
+(struct_e struct: (upper_ident) @identifier.type)
+(type_params (lower_ident) @identifier.type)
+(ty_var) @identifier.type
+(ty_cons (upper_ident) @identifier.type)
+(variant_pat
+    alternative: (upper_ident) @identifier.type
+    binder: (lower_ident) @identifier)
+
+(qualifier (upper_ident) @identifier.type)
+
+(var_pat (lower_ident) @identifier)
+
+(struct_field_top name: (lower_ident) @identifier.attribute)
+(struct_field_e name: (lower_ident) @identifier.attribute)
+(struct_idx_e index: (lower_ident) @identifier.attribute)
+(set_struct_idx index: (lower_ident) @identifier.attribute)
+
+(func_param name: (lower_ident) @identifier)
+
+(comment) @comment
